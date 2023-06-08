676ec4518bf086e58c4e70c9650e6323d731dd9e
DavidJoao <davidsandoval596@gmail.com>
2023-06-07 20:04:28 -0700

    SOLVED 2619

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//  1351
 // Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
 // Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
 // Output: 8
@@ -12,3 +14,21 @@ var countNegatives = function(grid) {
     
     return negativeArray.length;
 };
+
+//  2619 
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. 
+// If there are no elements in the array, it should return -1.
+// Input: nums = [1,2,3]
+// Output: 3
+// Explanation: Calling nums.last() should return the last element: 3.
+
+Array.prototype.last = function() {
+    return this.length === 0 ? -1 : this[this.length - 1]
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
+
+
