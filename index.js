096a805751cea6489bb01e5ba0be352955cb6743
DavidJoao <davidsandoval596@gmail.com>
2023-06-10 14:02:49 -0700

    SOLVED 27

--- index.js
+++ index.js
@@ -119,3 +119,23 @@ var removeDuplicates = function (nums) {
 
    return k
 }
+
+//      27
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+// The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+// The remaining elements of nums are not important as well as the size of nums.
+
+// Return k.
+
+var removeElement = function(nums, val) {
+    const filteredArray = nums.filter(numbers => numbers !== val)
+    // We want to return modified nums instead of new Array
+    nums.length = filteredArray.length;
+    filteredArray.forEach((number, index) => {
+        nums[index] = filteredArray[index]
+    })
+
+    return nums.length
+};
