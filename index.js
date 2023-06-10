cf32c8886c38bf8b78315ec79b39e6e7f3f65868
DavidJoao <davidsandoval596@gmail.com>
2023-06-10 13:39:58 -0700

    SOLVED 26

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-
 //  1351
 // Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
 // Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
 // Output: 8
 // Explanation: There are 8 negatives number in the matrix.
 
-var countNegatives = function(grid) {
-    let newGrid = grid.flat(grid.length);  
-    let negativeArray = [];
-    newGrid.forEach(number => {
-        if (number < 0) negativeArray.push(number)
-    });
-    
-    return negativeArray.length;
-};
+var countNegatives = function (grid) {
+   let newGrid = grid.flat(grid.length)
+   let negativeArray = []
+   newGrid.forEach(number => {
+      if (number < 0) negativeArray.push(number)
+   })
 
-//  2619 
-// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. 
+   return negativeArray.length
+}
+
+//  2619
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element.
 // If there are no elements in the array, it should return -1.
 // Input: nums = [1,2,3]
 // Output: 3
 // Explanation: Calling nums.last() should return the last element: 3.
 
-Array.prototype.last = function() {
-    return this.length === 0 ? -1 : this[this.length - 1]
-};
+Array.prototype.last = function () {
+   return this.length === 0 ? -1 : this[this.length - 1]
+}
 
 /**
  * const arr = [1, 2, 3];
  * arr.last(); // 3
  */
-
 
 //  2621
 // Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
@@ -43,14 +41,13 @@ Array.prototype.last = function() {
 // });
 
 async function sleep(millis) {
-    return new Promise (resolve => setTimeout(resolve, millis))
+   return new Promise(resolve => setTimeout(resolve, millis))
 }
 
-/** 
+/**
  * let t = Date.now()
  * sleep(100).then(() => console.log(Date.now() - t)) // 100
  */
-
 
 //  2529
 // Given an array nums sorted in non-decreasing order, return the maximum between the number of positive integers and the number of negative integers.
@@ -61,28 +58,25 @@ async function sleep(millis) {
 // Output: 3
 // Explanation: There are 3 positive integers and 3 negative integers. The maximum count among them is 3.
 
-var maximumCount = function(nums) {
-    let positiveNums = [];
-    let negativeNums = [];
-    nums.forEach(num => {
-        if (num > 0) {
+var maximumCount = function (nums) {
+   let positiveNums = []
+   let negativeNums = []
+   nums.forEach(num => {
+      if (num > 0) {
+         positiveNums.push(num)
+      } else if (num < 0) {
+         negativeNums.push(num)
+      } else if (num === 0) {
+         let zeroArray = nums.filter(num => num === 0)
+         if (zeroArray.length === 0) {
             positiveNums.push(num)
-        } else if (num < 0) {
-            negativeNums.push(num)
-        } else if (num === 0) {
-            let zeroArray = nums.filter(num => num === 0);
-            if (zeroArray.length === 0) {
-                positiveNums.push(num);
-            } else if (zeroArray.length >= 1) {
-                
-            }
-        }
-    })
+         } else if (zeroArray.length >= 1) {
+         }
+      }
+   })
 
-    return positiveNums.length > negativeNums.length ? positiveNums.length : negativeNums.length
-
-};
-
+   return positiveNums.length > negativeNums.length ? positiveNums.length : negativeNums.length
+}
 
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
@@ -90,14 +84,38 @@ var maximumCount = function(nums) {
 
 // You can return the answer in any order.
 
+var twoSum = function (nums, target) {
+   // First start iterating over array
+   for (let i = 0; i < nums.length; i++) {
+      // Then for each number in array, we will iterate again to check next number
+      for (let j = i + 1; j < nums.length; j++) {
+         // For each of i Iteration, we will iterate all j, so check if i + j === target
+         if (nums[i] + nums[j] === target) return [i, j]
+      }
+   }
+}
 
-var twoSum = function(nums, target) {
-    // First start iterating over array
-    for (let i = 0; i < nums.length; i++){
-        // Then for each number in array, we will iterate again to check next number
-        for (let j = i + 1; j < nums.length; j++){
-            // For each of i Iteration, we will iterate all j, so check if i + j === target
-             if (nums[i] + nums[j] === target) return [i, j]
-        }
-    }
-};
+//  26
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once.
+// The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially.
+// The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+
+var removeDuplicates = function (nums) {
+   if (nums.length === 0) {
+      return 0 // If the array is empty, there are no unique elements
+   }
+
+   let k = 1 // Initialize the count of unique elements to 1 (assuming at least one element is present)
+
+   for (let i = 1; i < nums.length; i++) {
+      if (nums[i] !== nums[i - 1]) {
+         nums[k] = nums[i] // Update the next unique element in the array
+         k++ // Increment the count of unique elements
+      }
+   }
+
+   return k
+}
