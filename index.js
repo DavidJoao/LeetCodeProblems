677efec2c64eca1b3635397dd449914b75d285f4
DavidJoao <davidsandoval596@gmail.com>
2023-06-13 15:02:49 -0700

    SOLVED 412

--- index.js
+++ index.js
@@ -223,3 +223,33 @@ var argumentsLength = function(...args) {
    for (let arg of args) sum += 1;
    return sum;
 };
+
+// 412. Fizz Buzz
+// Given an integer n, return a string array answer (1-indexed) where:
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+
+var fizzBuzz = function(n) {
+   let resultArray = [];
+   for (let i = 1; i <= n; i++){
+       switch (true) {
+           case i % 3 === 0 && i % 5 === 0:
+               resultArray.push('FizzBuzz');
+               break;
+           case i % 3 === 0:
+               resultArray.push('Fizz');
+               break;
+           case i % 5 === 0:
+               resultArray.push('Buzz');
+               break;
+           default:
+               resultArray.push(i.toString());
+               break;
+       }
+   }
+
+   return resultArray
+};
