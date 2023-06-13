d38b2c59f1cca30f73db8b85887485461fd0001c
DavidJoao <davidsandoval596@gmail.com>
2023-06-12 21:18:26 -0700

    SOLVED 28

--- index.js
+++ index.js
@@ -139,3 +139,11 @@ var removeElement = function(nums, val) {
 
     return nums.length
 };
+
+//      28
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+var strStr = function(haystack, needle) {
+    const result = haystack.indexOf(needle);
+    return result === undefined ? -1 : result;
+};
