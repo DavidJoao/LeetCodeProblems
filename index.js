39a95f18b54025dd7e3f48abf2a41d50ad6b28ca
DavidJoao <davidsandoval596@gmail.com>
2023-06-13 14:45:27 -0700

    SOLVED 2667

--- index.js
+++ index.js
@@ -189,3 +189,23 @@ var plusOne = function(digits) {
 var plusOne = function(digits) {
    return (parseInt(digits.join('')) + 1).toString().split('')
 };
+
+// 2667. Create Hello World Function
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+ 
+
+// Example 1:
+
+// Input: args = []
+// Output: "Hello World"
+// Explanation:
+// const f = createHelloWorld();
+// f(); // "Hello World"
+
+// The function returned by createHelloWorld should always return "Hello World".
+
+var createHelloWorld = function() {
+   return function(...args) {
+       return 'Hello World'
+   }
+};
