7144bad8245ef27ef5bae80b77e55719a47afdc9
DavidJoao <davidsandoval596@gmail.com>
2023-06-14 13:12:03 -0700

    SOLVED 2727

--- index.js
+++ index.js
@@ -274,3 +274,14 @@ var createCounter = function(n) {
        return n + count - 1
    }
 };
+
+
+// 2727. Is Object Empty
+// Given an object or an array, return if it is empty.
+// An empty object contains no key-value pairs.
+// An empty array contains no elements.
+// You may assume the object or array is the output of JSON.parse.
+
+var isEmpty = function(obj) {
+   return Object.keys(obj).length === 0;
+};
