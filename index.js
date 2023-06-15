b6bbafe47b4d226fc7f9a86e1623a1421ca27532
DavidJoao <davidsandoval596@gmail.com>
2023-06-15 13:17:07 -0700

    SOLVED 242

--- index.js
+++ index.js
@@ -285,3 +285,16 @@ var createCounter = function(n) {
 var isEmpty = function(obj) {
    return Object.keys(obj).length === 0;
 };
+
+// 242. Valid Anagram
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+var isAnagram = function(s, t) {
+   let firstStringArray = s.split('').sort().join('')
+   let secondStringArray = t.split('').sort().join('')
+   return firstStringArray === secondStringArray;
+};
